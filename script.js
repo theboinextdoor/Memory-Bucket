f8db4c5d329c5632cf1fb17a6529c1c127250f3a
Damon <cman55245@gmail.com>
2023-10-26 17:54:29 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const myBtn = document.querySelector(".btn");
+const noteContainer = document.querySelector(".noteContainer");
+let notes = document.querySelectorAll(".input-box");
+
+function getData(){
+    noteContainer.innerHTML = localStorage.getItem("notes");
+}
+getData();
+
+function showData(){
+    localStorage.setItem("notes", noteContainer.innerHTML);
+}
+
+myBtn.addEventListener("click", ()=>{
+    let inputBox = document.createElement("p");
+    let image = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    image.src= "images/trash-bin.png";
+    noteContainer.appendChild(inputBox).appendChild(image);
+})
+
+noteContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove()
+        showData();
+    }else if(e.target.tagName === "P"){
+        notes= document.querySelectorAll(".input-box");
+        notes.forEach( nt=>{
+            nt.onkeyup= function(){
+                showData();
+            }
+        })
+    }
+})
+
+
